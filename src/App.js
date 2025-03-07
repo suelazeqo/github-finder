719ee255cf5744e2b2ce6f7583cd8f7babf329ae
suelazeqo <suelazeqo19@gmail.com>
2025-03-07 10:22:27 +0100

    Implemented Alert logic

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import {GithubProvider} from "./context/github/GithubContext";
+import {AlertProvider} from "./context/alert/AlertContext";
+import Alert from "./components/layouts/Alert";
 
 function App() {
     return (
         <GithubProvider>
+            <AlertProvider>
             <Router>
                 <div className="flex flex-col justify-between h-screen">
                     <Navbar/>
                     <main className='container mx-auto px-3 pb-12'>
+                      <Alert/>
                         <Routes>
                             <Route path='/' element={<Home/>}/>
                             <Route path='/about' element={<About/>}/>
@@ -23,6 +27,7 @@ function App() {
                     <Footer/>
                 </div>
             </Router>
+            </AlertProvider>
         </GithubProvider>
     );
 }
