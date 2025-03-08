1030c88b69aff8532e8fb55311a911a91453c7a3
suelazeqo <suelazeqo19@gmail.com>
2025-03-08 12:25:16 +0100

    Created user profile

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,6 +8,8 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
+        user:{},
+        repos:[],
         loading: false
     }
 
@@ -25,13 +27,48 @@ export const GithubProvider = ({children}) => {
         })
     }
 
+    const getUser = async (login) => {
+        setLoading();
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}`);
+
+        if(response.status === 404){
+            window.location = '/notfound'
+        } else {
+            const data= await response.json();
+            dispatch({
+                type: 'GET_USER',
+                payload: data
+            })
+        }
+    }
+
+    const getUserRepos = async (login) => {
+        setLoading();
+
+        const params = new URLSearchParams({
+            sort:'created',
+            per_page: 10
+        })
+        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`);
+        const data = await response.json();
+        dispatch({
+            type: 'GET_REPOS',
+            payload: data
+        })
+    }
+
     const clearUsers = () => dispatch({type:'CLEAR_USERS'})
     const setLoading = () => dispatch({type: 'SET_LOADING'})
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
+        user:state.user,
+        repos: state.repos,
         searchUsers,
-        clearUsers
+        clearUsers,
+        getUser,
+        getUserRepos
     }}>
         {children}
     </GithubContext.Provider>
--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -4,12 +4,25 @@ const githubReducer = (state, action) => {
             return {
                 ...state,
                 users: action.payload,
-                loading:false
+                loading: false
+            }
+        case 'GET_USER':
+            return {
+                ...state,
+                user: action.payload,
+                loading: false
+            }
+
+        case 'GET_REPOS':
+            return {
+                ...state,
+                repos: action.payload,
+                loading: false
             }
         case 'SET_LOADING':
             return {
                 ...state,
-                loading:true
+                loading: true
             }
         case 'CLEAR_USERS':
             return {
